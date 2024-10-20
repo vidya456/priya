41e3a95959407ee041c6551ae0fa58ca3d812414
medaharipriya2001@gmail.com <ec2-user@ip-172-31-85-186.ec2.internal>
2024-10-20 15:48:17 +0000

    1st commit

---
+++ first.java
@@ -0,0 +1,10 @@
+public class student_login
+{ 
+	public static void main(string args[])
+        {
+		system.out.println("vansro technologies");
+	        system.out.println("student login page");
+		system.out.println("enter your user ID:");
+		system.out.println("enter your password:");
+	}
+}
